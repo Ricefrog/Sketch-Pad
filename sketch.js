14a6c617aab494c2f03d7eec02841536c00bd064
Koa Calloway <koacalloway@gmail.com>
2020-01-27 23:05:04 -0800

    Added a navbar, settings and erase button. Erase button works. Added automatic initialization to 16*16. Added shading to grid-items.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let dimensions = 16;
+init(dimensions);
 
 //function to initialize the board
 function init(num) {
@@ -19,7 +20,7 @@ function init(num) {
 };
 
 //function for soft-resetting the board
-function reset() {
+function softReset() {
     const board = document.querySelector(".board-container");
     const gridArr = document.querySelectorAll(".grid");
 
@@ -38,5 +39,33 @@ function color(currentColor) {
         let b = Math.random() * 255;
         
         return `rgb(${r}, ${g}, ${b})`;
-    }
+    } else { //darken the shade
+        let shade = 0.8;
+        let rgb = rgbGet(currentColor);
+        let r = rgb[0];
+        let g = rgb[1];
+        let b = rgb[2];
+
+        r *= shade;
+        g *= shade;
+        b *= shade;
+
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+};
+
+//addEventListener to erase button
+const eraser = document.querySelector(".fa-eraser");
+eraser.addEventListener("click", softReset);
+
+//function to get rgb values from a string
+function rgbGet(currentColor) {
+    let str = currentColor.slice(4);
+    let r = str.slice(0, str.indexOf(','));
+    str = str.slice(str.indexOf(',')+2);
+    let g = str.slice(0, str.indexOf(','));
+    str = str.slice(str.indexOf(',')+2);
+    let b = str.slice(0, str.indexOf(')'));
+    
+    return [r, g, b];
 };
